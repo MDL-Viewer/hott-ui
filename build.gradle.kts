f82a72b66a3d0ebe19fc8f1aff487a61e9eabc7c
Oliver Treichel <oliver.treichel@gmx.de>
2024-01-27 19:38:54 +0100

    feat: build as Java module

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,13 @@ semver {
 version = semver.version
 
 tasks {
+    compileJava {
+        options.compilerArgumentProviders.add(CommandLineArgumentProvider {
+            // Provide compiled Kotlin classes to javac – needed for Java/Kotlin mixed sources to work
+            listOf("--patch-module", "de.treichels.hott.ui=${sourceSets["main"].output.asPath}")
+        })
+    }
+
     jar {
         manifest {
             attributes (
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module de.treichels.hott.ui {
+    requires kotlin.stdlib;
+    requires de.treichels.hott.util;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.web;
+    requires java.logging;
+    requires java.desktop;
+    exports de.treichels.hott.ui;
+}
